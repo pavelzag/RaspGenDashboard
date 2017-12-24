ee1907cfa9cb7ec9ba9c80356006d4c55921b5f9
pavelzag <zagalsky@gmail.com>
2017-12-24 17:26:01 +0200

    Added Heroku switcher to configuration

--- configuration.py
+++ configuration.py
@@ -7,17 +7,13 @@ with open(config_path, 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
 
-def get_config(parameter_name):
-    return cfg['creds'][parameter_name]
-
-
-def get_white_list():
-    return cfg['white_list']
-
-
-def get_pin():
-    return cfg['pin']
-
-
 def get_db_creds(parameter_name):
-    return cfg['mongodb'][parameter_name]
+    if 'DYNO' in os.environ:
+        is_heroku = True
+    else:
+        is_heroku = False
+
+    if is_heroku:
+        return os.environ.get(parameter_name, 'Theres\'s nothing here')
+    else:
+        return cfg['mongodb'][parameter_name]
